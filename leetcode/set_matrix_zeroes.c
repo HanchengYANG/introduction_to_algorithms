83bbe3396358616d0f3881db18694d34373dc6f4
Hancheng Yang <hancheng.yang@outlook.fr>
2021-11-20 12:00:37 +0100

    Make better set_matrix_zeroes
    Do not use extra memories, do it in place

--- leetcode/set_matrix_zeroes.c
+++ leetcode/set_matrix_zeroes.c
@@ -9,52 +9,55 @@ void print_matrix(int** matrix, int matrixSize, const int* matrixColSize) {
     int i, j;
     for (i = 0; i < matrixSize; i++) {
         for (j = 0; j < matrixColSize[i]; j++)
-            printf("%d ", matrix[i][j]);
+            printf("%d\t", matrix[i][j]);
         printf("\n");
     }
 }
 
 void setZeroes(int** matrix, int matrixSize, int* matrixColSize) {
-    int i, j;
-    //int* row_zero = (int*)malloc(matrixColSize[0] * sizeof(int ));
-    //int* col_zero = (int*)malloc(matrixSize * sizeof(int ));
-    int row_zero[20];
-    int col_zero[20];
+    int i, j, tmp = matrix[0][0], row_size = matrixColSize[0];
     for (i = 0; i < matrixSize; i++) {
-        for (j = 0; j < matrixColSize[i]; j++) {
+        for (j = 0; j < row_size; j++) {
             if (matrix[i][j] == 0) {
-                row_zero[j] = 99;
-                col_zero[i] = 99;
+                matrix[i][0] = 0;
+                if (j == 0)
+                    tmp = 0; // Represents if the first column has 0
+                else
+                    matrix[0][j] = 0;
             }
         }
     }
-    for (j = 0; j < matrixColSize[0]; j++) {
-        if (row_zero[j] == 99) {
-            for (i = 0; i < matrixSize; i++) {
+
+    for (i = 1; i < matrixSize; i++) {
+        for (j = 1; j < row_size; j++) {
+            // j cannot be 0, so tmp is not used
+            if(matrix[0][j] == 0 || matrix[i][0] == 0)
                 matrix[i][j] = 0;
-            }
         }
     }
-    for (i = 0; i < matrixSize; i++) {
-        if (col_zero[i] == 99) {
-            for (j = 0; j < matrixColSize[0]; j++) {
-                matrix[i][j] = 0;
-            }
-        }
+
+    // check first row
+    if (matrix[0][0] == 0) {
+        for (j = 1; j < row_size; j++)
+            matrix[0][j] = 0;
     }
+
+    // check first column
+    if (tmp == 0) {
+        for (i = 1; i < matrixSize; i++)
+            matrix[i][0] = 0;
+    }
+    if (tmp == 0)
+        matrix[0][0] = 0;
 }
 
 int main() {
-    int row0[] = {8,7,7,4,1,6};
-    int row1[] = {7,5,4,6,8,8};
-    int row2[] = {0,9,4,3,0,5};
-    int row3[] = {9,3,5,8,7,5};
-    int row4[] = {0,1,0,8,9,7};
-    int row5[] = {5,7,5,4,1,8};
-    int row6[] = {2,0,6,7,7,4};
-    int row7[] = {7,7,5,1,0,8};
-    int* m[] = {row0, row1, row2, row3, row4, row5, row6, row7};
-    int col_size[] = {6,6,6,6,6,6,6,6};
+    int row0[] = {1,2,3,4};
+    int row1[] = {5,0,7,8};
+    int row2[] = {0,10,11,12};
+    int row3[] = {13,14,15,0};
+    int* m[] = {row0, row1, row2, row3};
+    int col_size[] = {4,4,4,4};
     print_matrix(m, sizeof(m)/sizeof(int*), col_size);
     setZeroes(m, sizeof(m)/sizeof(int*), col_size);
     printf("Done\n");
